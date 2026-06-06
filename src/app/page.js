6644d42938137bcaab81310988e56cfc645aecc5
Esther Chukwunyerenwa <zeeuxrd@gmail.com>
2026-06-06 22:27:27 +0100

    feat: add Footer component to the home page layout

--- src/app/page.js
+++ src/app/page.js
@@ -1,9 +1,10 @@
-import Pricing from "../components/Pricing";
+import Footer from "../components/Footer";
 
 export default function Home() {
   return (
-    <div className="flex flex-col min-h-screen">
-      <Pricing />
+    <div className="flex flex-col min-h-screen bg-[#1A1A1A] p-2 md:p-[40px] overflow-x-hidden">
+      {/* The Custom Footer */}
+      <Footer />
     </div>
   );
 }
