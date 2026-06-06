170d5bd0641aafc561d9081ac1243b85eba6e1c5
Esther Chukwunyerenwa <zeeuxrd@gmail.com>
2026-06-06 22:12:59 +0100

    initial commit

--- src/app/page.js
+++ src/app/page.js
@@ -1,10 +1,9 @@
-import Footer from "../components/Footer";
+import Pricing from "../components/Pricing";
 
 export default function Home() {
   return (
-    <div className="flex flex-col min-h-screen bg-[#1A1A1A] p-2 md:p-[40px] overflow-x-hidden">
-      {/* The Custom Footer */}
-      <Footer />
+    <div className="flex flex-col min-h-screen">
+      <Pricing />
     </div>
   );
 }
