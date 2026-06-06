1b3c65c018163c11ccdfa403d49028077ead2d96
Esther Chukwunyerenwa <zeeuxrd@gmail.com>
2026-06-06 10:01:12 +0100

    Refine mobile responsive padding to 8px

--- src/app/page.js
+++ src/app/page.js
@@ -2,7 +2,7 @@ import Footer from "../components/Footer";
 
 export default function Home() {
   return (
-    <div className="flex flex-col min-h-screen bg-[#1A1A1A] p-4 md:p-[40px] overflow-x-hidden">
+    <div className="flex flex-col min-h-screen bg-[#1A1A1A] p-2 md:p-[40px] overflow-x-hidden">
       {/* The Custom Footer */}
       <Footer />
     </div>
